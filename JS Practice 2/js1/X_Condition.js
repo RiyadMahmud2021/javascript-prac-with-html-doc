102056e376109c95592fec93abd6915442b70005
Riyad Mahmud <79051584+RiyadMahmud2021@users.noreply.github.com>
2021-08-20 16:48:43 +0600

    Delete JS Practice 2 directory

--- JS Practice 2/js1/X_Condition.js
+++
@@ -1,5 +0,0 @@
-// --------------------------Need to Understand--------------------------------------------------
-// Ternary Condition
-var number = Number(prompt("Enter a number "));
-var result = number>0 ? "positive" : number<0 ? "Negative" : "zero";
-console.log(result); 
